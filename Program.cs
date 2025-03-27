34736c21509e27f56857acfa4aea8d98c45e6a4f
Co Nguyen <nguye2co@mail.uc.edu>
2025-03-27 11:43:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass03_27_2025
+{
+    internal class Program
+    {
+        private double _price;
+
+        // Create getter and a setter for the private properties
+        public double Price { get { return _price; } set { _price = value; } }
+
+
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
